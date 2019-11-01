b7a10e5ef05c70f3136e82ce9691a8a0921592ba
Mykola Solonyy <nicksolony@gmail.com>
2019-11-01 00:42:45 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,22 @@
+director_1 = {
+  name: "Ida Lupino",
+  movie: "The Hitch-Hiker"
+}
+
+director_2 ={
+  name: "Orson Welles",
+  movie: "Citizen Kane"
+}
+
+director_3 = {
+  name: "Andrej Tarkovsky",
+  movie: "Solaris"
+}
+
+film_fest = [
+  director_1,
+  director_2,
+  director_3
+  ]
+  
+  puts film_fest
