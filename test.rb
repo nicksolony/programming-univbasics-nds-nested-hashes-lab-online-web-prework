bfc35efcb99ea3f4d2c8eadbeb0e9c8820914682
Mykola Solonyy <nicksolony@gmail.com>
2019-11-01 00:59:05 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,31 @@
+director_1 = {
+  name: "Ida Lupino",
+  movie: "The Hitch-Hiker"
+}
+
+director_2 ={
+  name: "Orson Welles",
+  movie: "Citizen Kane"
+}
+
+director_3 = {
+  name: "Andrej Tarkovsky",
+  movie: "Solaris"
+}
+
+film_fest = [
+  director_1,
+  director_2,
+  director_3
+  ]
+  
+ puts  film_fest
+  
+  #film_fest[0][:name] = "John Ford"
+  #film_fest[0][:movie] = "The grapes of wrath"
+  
+  film_fest[1] = { name:"Cecil DeMille",
+  movie: "Samson"}
+  
+puts  
+ puts  film_fest
